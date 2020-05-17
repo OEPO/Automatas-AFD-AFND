86345472064070bd0277aced54b00a270410a84c
Daniela Galleguillos <dangalledi@gmail.com>
2020-05-16 20:43:45 -0400

    ADD: Secret key

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess' 
+    
+    #  Flask y algunas de sus extensiones utilizan el valor de la clave secreta como clave criptográfica, útil para generar firmas o tokens. La extensión Flask-WTF lo utiliza para proteger los formularios web contra un ataque llamado Cross-Site Request Forgery o CSRF
